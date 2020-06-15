d23a4b22dabe5b1de1f2a009c6f46e25b3df384b
MindaugasK <mindaugas.krup@gmail.com>
2020-06-15 13:53:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 
 using namespace std;
 
-static string address = "Mesoamerican pyramids - Wikipedia.txt";
+static string address = "data.txt";
 static string text; //pilnas tekstas, 1000+ zodziu
 static map<string, vector<int> > FoundAt; //asociatyvus zymejimas
+static map<string, vector<int> > userPhase;
 static vector<string> Url;
 
 void import(string address);
@@ -20,20 +21,25 @@ void import(string address);
 void getURLs();
 void filter();
 void print();
-void search();
+//void search();
+void search(string s);
 
 int main() {
     //deklaracijos
-
+    string userinput;
     //pradzia
     import(address);
+
+    //setlocale(LC_ALL, "Lithuanian");
+
+    getline(cin, userinput);
 
     getURLs();
     filter();
     for(string h : Url){
         cout << h << endl;
     }
-    search();
+    search(userinput);
     cout << endl << endl << endl;
     print();
     system("PAUSE");
@@ -51,7 +57,7 @@ void import(string address){
 }
 
 void print(){
-    cout << text;
+    //cout << text;
     cout << endl << endl;
     cout << "================================================================================================" << endl;
     for(auto it = FoundAt.begin(); it != FoundAt.end(); ++it){
@@ -63,8 +69,23 @@ void print(){
                 cout << I << " , ";
             }
             cout << endl;
+            cout << "----------------------------------------------------------------------------------------------------------------------" << endl;
         }
 
+    }
+
+    cout << endl << endl;
+    cout << "Naudotojo ivestos frazes zodziai: " << endl << endl;
+    for(auto it2 = userPhase.begin(); it2 != userPhase.end(); ++it2){
+        if(it2-> first != " " && it2->first != ""){
+            cout << it2->first << " : ";
+            cout << " [Isrinkta: " << it2->second.size() << " ] : " << endl;
+            set<int> H2(it2->second.begin(), it2->second.end());
+            for(int I2 : H2){
+                cout << I2 << " , ";
+            }
+        }
+        cout << endl;
     }
 }
 
@@ -161,6 +182,56 @@ void search(){
             }
         }
         Q++;
+    }
+
+    cout << "Q = " << Q << endl;
+}
+
+void search(string M){ // M - fraze
+    set<string> zodziai;
+    istringstream m(M);
+    string zodis = "";
+    while(!m.eof()){
+        m >> zodis;
+        transform(zodis.begin(), zodis.end(), zodis.begin(), ::tolower);
+        zodziai.insert(zodis);
+    }
+
+
+    istringstream s(text);
+    set<string> words;
+    int Q = 0;
+    string line = "";
+    while(getline(s, line)){
+        istringstream linestream(line);
+        string a;
+        while(linestream >> a){
+
+            transform(a.begin(), a.end(), a.begin(), ::tolower);
+            typedef pair<_Rb_tree_const_iterator<string>, bool> setResult;
+            setResult B = words.insert(a);
+
+            if(!B.second){
+                vector<int> fnum;
+                fnum.push_back(Q);
+                pair<string, vector<int> > p = make_pair(a, fnum);
+                FoundAt.insert(p);
+            } else {
+                for(auto it = FoundAt.begin(); it != FoundAt.end(); ++it){
+                    it->second.push_back(Q);
+                }
+            }
+        }
+        Q++;
+    }
+
+    for(string s : zodziai){
+        cout << s << endl;
+        for(pair<string, vector<int> > p : FoundAt){
+            if(p.first == s){
+                userPhase.insert(p);
+            }
+        }
     }
 
     cout << "Q = " << Q << endl;
